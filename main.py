9cbc7ad07932146c3de95ddb9ee1ae896ad69fe0
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-08 22:17:45 +0000

    added many body generation

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def returnAcceleration(pos,vel, masses, detectCollisionsOf):
             detR = (relX**2 + relY**2 + relZ**2 + softening**0.2)**(-3/2)
             dis = (relX**2 + relY**2 + relZ**2)**0.5
             # the relX makes sure that the accel due to itself becomes 0
-            a[i,0] += 6.67*10**(-11) * relX * detR *  masses[j] # note the +=. We are summing up the accels for each particle (N) rel to every other particle (N). so it comes to N**2
-            a[i,1] += 6.67*10**(-11) * relY * detR *  masses[j]
-            a[i,2] += 6.67*10**(-11) * relZ * detR *  masses[j]
+            a[i,0] += 6.67*10**(2) * relX * detR *  masses[j] # note the +=. We are summing up the accels for each particle (N) rel to every other particle (N). so it comes to N**2
+            a[i,1] += 6.67*10**(2) * relY * detR *  masses[j]
+            a[i,2] += 6.67*10**(2) * relZ * detR *  masses[j]
             # detecting any specified collisons
             if (set([i,j])==set(detectCollisionsOf)) and dis<0.1:
                 stop = True
@@ -38,22 +38,34 @@ def returnAcceleration(pos,vel, masses, detectCollisionsOf):
 pos = np.array(
     [
         [0,0,0],
-        [150*10*9,0,0],
+        [10,0,0],
+        [5,5,0],
+        [3,3,3]
     ]
 )
 vel =  np.array(
     [
+        [0,10,0],
         [0,0,0],
-        [0,30000000,0],
+        [0,0,0],
+        [0,0,0]
     ]
 )
-masses = [2*(10**30), 6*(10**24)]
-step = 0.000001
+masses = [1000,1000,1000,1]
+step = 0.001
+
+numParticles = 100
+pos2 = np.random.rand(numParticles,3)
+vel2 = np.random.rand(numParticles,3)
+masses2 = np.random.rand(numParticles)
+print(pos2)
 #------------------------
 
 def nBodySimulator(pos, vel, masses, step, detectCollisionsOf, displayEnergy):
 
-    colors = ['red','green', 'blue', 'magenta', 'violet', 'orange', 'yellow', 'cyan']
+    colors = []
+    for i in range(200):
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
     N = pos.shape[0]
 
     # Initial accelerations
@@ -89,10 +101,10 @@ def nBodySimulator(pos, vel, masses, step, detectCollisionsOf, displayEnergy):
         if keyboard.is_pressed('c') or stop==True:
             break
         plt.pause(0.01)
-        #plot.remove()
+        plot.remove()
 
     posHistory = np.array(posHistory)
     plt.show()
 
 # ---- Function Call
-nBodySimulator(pos, vel, masses, step, [3,7], False)
+nBodySimulator(pos2, vel2, masses2, step, [3,7000], False)
