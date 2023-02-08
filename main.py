0808cacbbfe03eb78232f5cc2d3695d26570632e
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-08 00:59:20 +0000

    complete(?) programme

--- main.py
+++ main.py
@@ -15,21 +15,43 @@ import matplotlib  as plt
 
 def returnAcceleration(pos, mass):
     #pos = N x 3
-    #mas = N x 1
+    #mass = N x 1
+    softening = 0.1
     N = pos.shape[0]
-    a = np.zeros((N,3))
-    relXAll = np.zeros((N,N))
+    a = np.zeros((N,3)) # N x 3
     for i in range(N):
         for j in range(N):
             relX = pos[j,0] - pos[i,0]
-            relXAll[j,i]  = relX
-    print(relXAll)
-
+            relY = pos[j,1] - pos[i,1]
+            relZ = pos[j,2] - pos[i,2]
+            detR = (relX**2 + relY**2 + relZ**2 + softening**0.2)**(-3/2)
+            a[i,0] += 6.67*10**(2) * relX * detR *  mass # note the +=. We are summing up the accels for each particle (N) rel to every other particle (N). so it comes to N**2
+            a[i,1] += 6.67*10**(2) * relY * detR *  mass
+            a[i,2] += 6.67*10**(2) * relZ * detR *  mass
+    return a
+    
 pos = np.array(
     [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
+        [1,2,0],
+        [4,5,0],
+        # [7,8,0]
     ]
 )
-returnAcceleration(pos, 1)
+vel =  np.array(
+    [
+        [1,2,0],
+        [4,5,0],
+        # [7,8,0]
+    ]
+)
+mass = 100
+accel = returnAcceleration( pos, mass)
+for i in range(10):
+    vel = vel +  accel*0.01/2
+    pos = pos + vel*0.01
+    accel = returnAcceleration(pos, mass)
+    vel += accel*0.01/2
+    print('.....')
+    dis = pos[0,0:pos.shape[1]] - pos[1,0:pos.shape[1]]
+    # print((dis[0]**2 + dis[1]**2)**0.5)
+    print(pos)
